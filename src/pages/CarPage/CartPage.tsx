d13ad11414c041c33e2d24aa30cbba5315df9c91
Rokas Kamblevicius <rokaskamble39@gmail.com>
2023-11-04 17:51:08 +0200

    feat: add edit amount to cart page

--- src/pages/CarPage/CartPage.tsx
+++ src/pages/CarPage/CartPage.tsx
@@ -7,28 +7,67 @@ import {
   ListItemText,
   ListItemSecondaryAction,
   IconButton,
+  Grid,
+  Button,
+  TextField,
 } from '@mui/material';
 import { RemoveCircle } from '@mui/icons-material';
 import { Toy } from '../../Interfaces/Toy';
+import MenuBar from '../../Components/MenuBar/MenuBar';
 
 function CartPage() {
-  const { cart, removeFromCart } = useCart();
+  const { cart, removeFromCart, addToCart } = useCart();
   const handleRemoveItem = (item: Toy) => {
     removeFromCart(item);
   };
 
+  const handleIncrement = (item: Toy, quantity: number) => {
+    addToCart(item, quantity + 1);
+  };
+
+  const handleDecrement = (item: Toy, quantity: number) => {
+    if (quantity > 1) {
+      addToCart(item, quantity - 1);
+    } else {
+      removeFromCart(item);
+    }
+  };
+
   return (
     <div>
+      <MenuBar />
       <Typography variant="h4" gutterBottom>
         Cart Page
       </Typography>
       <List>
         {cart.map((cartItem) => (
           <ListItem key={cartItem.item.id}>
-            <ListItemText
-              primary={cartItem.item.name}
-              secondary={`Quantty: ${cartItem.quantity}`}
-            />
+            <ListItemText primary={cartItem.item.name} />
+            <Grid container spacing={2} alignItems="center">
+              <Grid item>
+                <Button
+                  variant="contained"
+                  onClick={() =>
+                    handleDecrement(cartItem.item, cartItem.quantity)
+                  }
+                >
+                  -
+                </Button>
+              </Grid>
+              <Grid item>
+                <TextField type="number" value={cartItem.quantity} />
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  onClick={() =>
+                    handleIncrement(cartItem.item, cartItem.quantity)
+                  }
+                >
+                  +
+                </Button>
+              </Grid>
+            </Grid>
             <ListItemSecondaryAction>
               <IconButton
                 edge="end"
